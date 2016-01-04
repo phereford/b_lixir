7094477c343eb59416114dff04bc65bea3435e95
Patrick Hereford <phereford@users.noreply.github.com>
2016-01-04 08:33:21 -0500

    Readme updates

--- mix.exs
+++ mix.exs
@@ -30,13 +30,16 @@ defmodule BLixir.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.1.1"},
-     {:phoenix_ecto, "~> 2.0"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.3"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.9"},
-     {:cowboy, "~> 1.0"}]
+    [
+      {:phoenix, "~> 1.1.1"},
+      {:phoenix_ecto, "~> 2.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:phoenix_html, "~> 2.3"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.9"},
+      {:cowboy, "~> 1.0"},
+      {:credo, "~> 0.2", only: [:dev, :test]}
+    ]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
